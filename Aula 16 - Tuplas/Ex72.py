5e1770ac95121196193437fe81050ebffe334633
PedroFS <64667742+G0dof@users.noreply.github.com>
2023-05-02 08:40:02 -0300

    Create Ex72.py

---
+++ Aula 16 - Tuplas/Ex72.py
@@ -0,0 +1,8 @@
+numExtenso = ("zero", "um", "dois", "três", "quatro", "cinco", "seis", "sete", "oito", "nove", "dez", "onze", "doze", "treze", "quatorze", "quinze", "dezesseis", "dezessete", "dezoito", "dezenove", "vinte")
+
+num = int(input("Digite um número entre 0 e 20: "))
+
+while(num > 20 or num < 0):
+	num = int(input("Tente novamente. Digite um número entre 0 e 20: "))
+
+print(f"Você digitou o número {numExtenso[num]}")
